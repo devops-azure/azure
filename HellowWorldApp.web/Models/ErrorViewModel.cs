6e1e2580ac8654c7d384b5f02147757387422ea5
venu <venu.yakkanti97@gmail.com>
2022-01-05 16:11:59 +0530

    first commit

---
+++ HellowWorldApp.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HellowWorldApp.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
